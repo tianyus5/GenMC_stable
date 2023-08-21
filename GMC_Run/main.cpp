b388aa1686e4186b4d29a306fef9264489a7bc29
Tianyu <tianyus5@illinois.edu>
2023-08-21 16:02:34 -0500

    comment

--- GMC_Run/main.cpp
+++ GMC_Run/main.cpp
@@ -12,10 +12,10 @@ SimCell sim_cell; // The simulation cell object. This object contains all the in
 int main(void) {
 	Session ses("INPUT"); // settings for MC run from input file
 	if (ses.use_states == true) { ses.add_spin_states("SPIN_STATES"); }
-	ses.fill_rule_list(); // Initalize the "Rules" that will be used to properly apply the ECI to each site in the simulation cell. Rules are defined as follows, [species (0,1,2 for Ni,Mn,In)]:[NA if monomer,dist of dymer, AB,AC,BC if trimer]:no-spin(0) or spin(1): value
+	ses.fill_rule_list(); // Initalize the "Rules" that will be used to properly apply the ECI to each site in the simulation cell.
 	ses.find_unique_dists(); // Find the list of distances used in mc_rules
-	// Create the simulation cell object. Arguments (POSCAR_file, dist_list, shape, species numbs, cutoff (currently unused), sim_type (also unused), phase_init (aust/mart), spin_init (AFM/FM/RAND), species_init (Ordered/Random), bool use_poscar)
-	sim_cell.initSimCell("POSCAR", ses); // Create and initalize the simulation cell
+	
+	sim_cell.initSimCell("POSCAR", ses); // Create the simulation cell object. Arguments (POSCAR_file, dist_list, shape, species numbs, cutoff (currently unused), sim_type (also unused), phase_init (aust/mart), spin_init (AFM/FM/RAND), species_init (Ordered/Random), bool use_poscar)
 	Sim sim(ses, sim_cell);
 	sim.start();
 	return 0;
