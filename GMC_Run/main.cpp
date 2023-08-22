5975d8520a9f32e97981ca726d672e02aceb34f6
Tianyu <tianyus5@illinois.edu>
2023-08-21 22:16:17 -0500

    mian

--- GMC_Run/main.cpp
+++ GMC_Run/main.cpp
@@ -14,7 +14,6 @@ int main(void) {
 	if (ses.use_states == true) { ses.add_spin_states("SPIN_STATES"); }
 	ses.fill_rule_list(); // Initalize the "Rules" that will be used to properly apply the ECI to each site in the simulation cell.
 	ses.find_unique_dists(); // Find the list of distances used in mc_rules
-	
 	sim_cell.initSimCell("POSCAR", ses); // Create the simulation cell object. Arguments (POSCAR_file, dist_list, shape, species numbs, cutoff (currently unused), sim_type (also unused), phase_init (aust/mart), spin_init (AFM/FM/RAND), species_init (Ordered/Random), bool use_poscar)
 	Sim sim(ses, sim_cell);
 	sim.start();
